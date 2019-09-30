416725315c1291139388cbbd78c1060596529a6e
DmacMcgreg <davejmcg@gmail.com>
2019-09-30 12:32:51 -0400

    Update main layout - use styled components - add round border

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import base from './base.css'
-import Container from './container'
+//import Container from './container'
 import Navigation from './navigation'
+import styled from 'styled-components'
+
+const Container = styled.div` 
+  maxWidth: 1180;
+  margin: '0 auto';
+  padding: 20px 60px 60px 60px;
+  border-radius: 40px;
+  max-height: 100vh;
+  height: 100vh;
+  overflow: hidden;
+  box-sizing: border-box;
+`
 
 class Template extends React.Component {
   render() {
