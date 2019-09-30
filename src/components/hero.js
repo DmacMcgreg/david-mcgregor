51761ae789f4a601fe43c96ef6a499861baf3ea9
DmacMcgreg <davejmcg@gmail.com>
2019-09-30 12:33:42 -0400

    Update Main Page - update hero to take new images - add queries to ask for images, banner

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,14 +2,31 @@ import React from 'react'
 import Img from 'gatsby-image'
 
 import styles from './hero.module.css'
+import styled from "styled-components"
 
-export default ({ data }) => (
-  <div className={styles.hero}>
-    <Img className={styles.heroImage} alt={data.name} fluid={data.heroImage.fluid} />
-    <div className={styles.heroDetails}>
-      <h3 className={styles.heroHeadline}>{data.name}</h3>
-      <p className={styles.heroTitle}>{data.title}</p>
-      <p>{data.shortBio.shortBio}</p>
-    </div>
-  </div>
-)
+const Hero = styled.div`
+  position: relative;
+  color: #fff;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  margin: -50px -8px 0 -8px;
+  border-radius: 40px 40px 10px 10px;
+`
+
+export default ({ author, logo, banner }) => {
+
+  const heroImage = banner.fluid || logo.fluid
+
+  return (
+    <Hero id="Hero">
+        <Img className={styles.heroImage} alt={author.name}  fluid={heroImage} />
+      <div className={styles.heroDetails}>
+        <h3 className={styles.heroHeadline}>{author.name}</h3>
+        <p className={styles.heroTitle}>{author.title}</p>
+        <p>{author.shortBio.shortBio}</p>
+      </div>
+    </Hero>
+  
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,31 +5,76 @@ import Helmet from 'react-helmet'
 import Hero from '../components/hero'
 import Layout from '../components/layout'
 import ArticlePreview from '../components/article-preview'
+import { WOW } from 'wowjs/dist/wow';
+import styled from 'styled-components'
+import Img from 'gatsby-image'
+
+
+const InnerContainer = styled.main`
+  border-radius: 40px;
+  background: #fff;
+  box-shadow: 5px -5px 34px 0px rgba(16, 19, 43, 0.3), 5px -5px 37px 0px rgba(60, 97, 155, 0.3), 5px -5px 112px -6px rgba(237, 214, 196, 0.3);
+  overflow: scroll;
+  max-height: 100%;
+  height: 100%;
+  scrollbar-gutter: auto;
+`
+
+const BackgroundImage = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+`
+
+const Wrapper = styled.section`
+`
 
 class RootIndex extends React.Component {
+  componentDidMount() {
+    if (typeof window !== 'undefined') { 
+      const wow = new WOW()
+      wow.init()
+    } 
+  }
+
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const posts = get(this, 'props.data.allContentfulBlogPost.edges')
     const [author] = get(this, 'props.data.allContentfulPerson.edges')
+    const logo = get(this, 'props.data.contentfulLogo.logo')
+    const logos = get(this, 'props.data.contentfulLogo.logos', [])
+    const backgroundImage = get(this, 'props.data.contentfulAsset.backgroundImage')
+
+    const banner = logos.find(logo => logo.title.match(/banner/))
 
     return (
       <Layout location={this.props.location} >
-        <div style={{ background: '#fff' }}>
-          <Helmet title={siteTitle} />
-          <Hero data={author.node} />
-          <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
+        <BackgroundImage>
+          <Img fluid={backgroundImage} />
+        </BackgroundImage>
+        <InnerContainer>
+          <Helmet>
+            <title>{siteTitle}</title>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css" />
+          </Helmet>
+          <Hero author={author.node} logo={logo} banner={banner} />
+          <Wrapper>
+            <h2 className="section-headline wow animated fadeInLeft">Recent articles</h2>
+
             <ul className="article-list">
               {posts.map(({ node }) => {
                 return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
+                  <li key={node.slug} className="wow animated fadeInUp">
+                    <ArticlePreview  article={node} />
                   </li>
                 )
               })}
             </ul>
-          </div>
-        </div>
+          </Wrapper>
+        </InnerContainer>
       </Layout>
     )
   }
@@ -52,7 +97,7 @@ export const pageQuery = graphql`
           publishDate(formatString: "MMMM Do, YYYY")
           tags
           heroImage {
-            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
+            fluid(maxWidth: 350, maxHeight: 196, cropFocus: CENTER) {
              ...GatsbyContentfulFluid_tracedSVG
             }
           }
@@ -64,7 +109,38 @@ export const pageQuery = graphql`
         }
       }
     }
-    allContentfulPerson(filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }) {
+    contentfulAsset(contentful_id: {eq: "7515J4ZYjKJ4djoBuNkYda"}) {
+      backgroundImage: fluid(
+              maxWidth: 1583,
+              maxHeight: 989,
+              resizingBehavior: SCALE
+            ) {
+              ...GatsbyContentfulFluid_tracedSVG
+      }
+    }
+    contentfulLogo(contentful_id: {eq: "LxRxthQzpfcds0MBd4qT3"}) {
+      logo: image {
+        title
+        fluid(
+              maxWidth: 1180,
+              maxHeight: 480,
+              resizingBehavior: SCALE
+            ) {
+              ...GatsbyContentfulFluid_tracedSVG
+        }
+      }
+      logos: images {
+        title
+        fluid(
+              maxWidth: 935,
+              maxHeight: 400,
+              resizingBehavior: FILL
+            ) {
+              ...GatsbyContentfulFluid_tracedSVG
+        }
+      }
+    }
+    allContentfulPerson(filter: { contentful_id: { eq: "6jBoLbM9sq47jpBtClMQz" } }) {
       edges {
         node {
           name
@@ -77,7 +153,6 @@ export const pageQuery = graphql`
               maxWidth: 1180
               maxHeight: 480
               resizingBehavior: PAD
-              background: "rgb:000000"
             ) {
               ...GatsbyContentfulFluid_tracedSVG
             }
